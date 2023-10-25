1f7a6172fd49ce738767c17d0f8c3ad4d16cd39b
Lee <66083991+HalcyonLegion@users.noreply.github.com>
2023-10-25 17:24:20 +0800

    Create endsWith.py

---
+++ Python/endsWith.py
@@ -0,0 +1,5 @@
+def solution(text, ending):
+    # Needs something that takes the input (above, done) then checks the ending characters to check if they are matching the original string
+    # essentially something like, if ending in text then true.This kind of worked but there's actually a method in Python called .endswith, crazy..
+    
+    return text.endswith(ending)
